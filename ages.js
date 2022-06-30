f582530e39e233ac31d34364efd52ff14f7ba5b1
adilzada <100288857+adilzada@users.noreply.github.com>
2022-07-01 03:17:55 +0400

    added some codes))

--- ages.js
+++ ages.js
@@ -1,5 +1,16 @@
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 const sortAges=ages.sort();
 console.log(sortAges)
-const last=sortAges.length-1;
-// console.log(last);
+const lastNumber=sortAges.length-1;
+// console.log(lastNumber);
+const minValue=sortAges[0];
+const maxValue=sortAges[lastNumber];
+console.log(minValue,maxValue);
+let sumValues=sortAges.reduce((x, y) => x + y);
+console.log("sum= " +sumValues);
+let middleValue=sumValues/sortAges.length;
+console.log('middleValue= '+middleValue);
+const betweenMinMiddle=Math.abs(minValue-middleValue);
+console.log(betweenMinMiddle.toFixed(1));
+const betweenMaxMiddle=Math.abs(maxValue-middleValue);
+console.log(betweenMaxMiddle.toFixed(1));
