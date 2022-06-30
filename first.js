3f039a6c31e0c9f7a86273ff4869835f1b518810
adilzada <100288857+adilzada@users.noreply.github.com>
2022-07-01 02:35:12 +0400

    added ages.js

--- first.js
+++ first.js
@@ -4,12 +4,23 @@
 // const arr=fourArrayAValues;
 // console.log(arr);
 
-const twoArrayAValues=Array(2).fill('A');
-const treeArrayBValues=Array(3).fill('B');
-const totalArray=twoArrayAValues.concat(treeArrayBValues);
-totalArray.push('C')
-// console.log(totalArray.indexOf('A'));
-// console.log(totalArray.toString('  '));
-// console.log(totalArray.join('  '));
-totalArray.pop()
-console.log(totalArray);
+// const twoArrayAValues=Array(2).fill('A');
+// const treeArrayBValues=Array(3).fill('B');
+// const totalArray=twoArrayAValues.concat(treeArrayBValues);
+// totalArray.push('C')
+// // console.log(totalArray.indexOf('A'));
+// // console.log(totalArray.toString('  '));
+// // console.log(totalArray.join('  '));
+// // totalArray.pop()
+// console.log(totalArray);
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'MongoDB',
+    'graszhx'
+  ];
+  console.log(webTechs[Math.floor(webTechs.length/2)]);
